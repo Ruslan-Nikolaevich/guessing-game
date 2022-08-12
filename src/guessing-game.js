32a384789012dc7aeea3b66ab5ae5874b31e368e
Ruslan-Nikolaevich <ruslan_nic@mail.ru>
2022-08-12 22:13:18 +0300

    complete test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+   
+    constructor() {
+        this.minimal = 0;
+        this.maximum = 0;
+        this.rezultat = 0;
+    }
 
     setRange(min, max) {
-
+       this.minimal = min;
+       this.maximum = max;
     }
 
     guess() {
-
+        this.rezultat = Math.round((this.minimal + this.maximum) / 2);
+        return this.rezultat;
     }
 
     lower() {
-
+        this.maximum = this.rezultat; 
     }
 
     greater() {
-
+        this.minimal = this.rezultat;
     }
 }
 
